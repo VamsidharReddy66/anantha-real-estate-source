52d616d1f1dd0383b01e444dc8b84d7322db7fdd
ananthaconsultancy <jvk.aconsultancy@gmail.com>
2026-09-06 16:23:06 +0530

    Wire buy sell locations intelligence and analytics routes

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,16 @@ import ProjectPage from "./pages/ProjectPage";
 import PropertiesPage from "./pages/PropertiesPage";
 import PropertyCategoryPage from "./pages/PropertyCategoryPage";
 import PropertyPage from "./pages/PropertyPage";
+import BuyPropertyPage from "./pages/BuyPropertyPage";
+import SellPropertyPage from "./pages/SellPropertyPage";
+import LocationsPage from "./pages/LocationsPage";
+import LocationPage from "./pages/LocationPage";
+import PropertyIntelligencePage from "./pages/PropertyIntelligencePage";
 import ContactPage from "./pages/ContactPage";
 import NotFound from "./pages/NotFound";
 import CentralWorld from "./pages/centralworld";
 import ScrollToTop from "./components/ScrollToTop";
+import AnalyticsPageView from "./components/AnalyticsPageView";
 
 const queryClient = new QueryClient();
 
@@ -26,6 +32,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <ScrollToTop />
+        <AnalyticsPageView />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<AboutPage />} />
@@ -36,6 +43,11 @@ const App = () => (
           <Route path="/properties" element={<PropertiesPage />} />
           <Route path="/properties/:category" element={<PropertyCategoryPage />} />
           <Route path="/property/:slug" element={<PropertyPage />} />
+          <Route path="/buy-property" element={<BuyPropertyPage />} />
+          <Route path="/sell-your-property" element={<SellPropertyPage />} />
+          <Route path="/locations" element={<LocationsPage />} />
+          <Route path="/locations/:slug" element={<LocationPage />} />
+          <Route path="/property-intelligence" element={<PropertyIntelligencePage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/centralworld" element={<CentralWorld />} />
           <Route path="*" element={<NotFound />} />
