e13e1c2fc57f590456215639f6b95e790ceee27f
ananthaconsultancy <jvk.aconsultancy@gmail.com>
2026-09-09 18:59:20 +0530

    Remove buy and sell pages from website routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ import ProjectPage from "./pages/ProjectPage";
 import PropertiesPage from "./pages/PropertiesPage";
 import PropertyCategoryPage from "./pages/PropertyCategoryPage";
 import PropertyPage from "./pages/PropertyPage";
-import BuyPropertyPage from "./pages/BuyPropertyPage";
-import SellPropertyPage from "./pages/SellPropertyPage";
 import LocationsPage from "./pages/LocationsPage";
 import LocationPage from "./pages/LocationPage";
 import PropertyIntelligencePage from "./pages/PropertyIntelligencePage";
@@ -56,8 +54,6 @@ const App = () =>
               element={<PropertyCategoryPage />}
             />
             <Route path="/property/:slug" element={<PropertyPage />} />
-            <Route path="/buy-property" element={<BuyPropertyPage />} />
-            <Route path="/sell-your-property" element={<SellPropertyPage />} />
             <Route path="/locations" element={<LocationsPage />} />
             <Route path="/locations/:slug" element={<LocationPage />} />
             <Route
